4906eadbfcb4a318fd835c5c7c54e9fb0a90d37d
gobi <gobi@hartwintech.com>
2019-12-07 15:28:13 +0530

    lesson-7 mapping components practice

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-// import Header from './components/Header';
-// import MainContent from './components/MainContent';
-// import Footer from './components/Footer';
-// import ToDo from './components/ToDo';
-import ContactCard from './components/ContactCard';
+// import ReactDom from 'react-dom';
 
-function App() {
+import Joke from './components/Joke';
+import jokeData from './data/jokeData';
 
+
+import Product from './components/Product';
+import productData from './data/productData';
+
+function App(){
+  const jokeComponents = jokeData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} /> )
+  const productComponents = productData.map(product => <Product key={product.id} product={product} /> )
   return (
-    <div className="contacts">
-        <ContactCard 
-            contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
-        />
-        
-        <ContactCard 
-            contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
-        />
-        
-        <ContactCard
-            contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
-        />
-        
-        <ContactCard 
-            contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
-        />
-        
+    <div>
+           {jokeComponents}   
+           {productComponents}
     </div>
   )
 }
-
-export default App;
+export default App
