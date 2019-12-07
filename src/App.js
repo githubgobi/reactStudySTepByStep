307b34d832a750980b23607a71e8d328811abe0c
gobi <gobi@hartwintech.com>
2019-12-07 14:31:11 +0530

    lesson-5 todo list added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+// import Header from './components/Header';
+// import MainContent from './components/MainContent';
+// import Footer from './components/Footer';
+import ToDo from './components/ToDo';
 
 function App() {
 
   return (
-      <div>
-        <Header />
-        <MainContent />
-        <Footer />
+      <div className="todo-list">
+        <ToDo />
+        <ToDo />
+        <ToDo />
+        <ToDo />
       </div>
   )
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,22 +3,28 @@ import React from 'react';
 function MainContent(){
     const firstName = "GOBI"
     const lastName = "R"
-    const date = new Date()
+    const date = new Date(2018, 6, 31, 15)
     const hours = date.getHours()
     let timeOfDay
+    const styles = {
+        fontSize:30
+    }
     if(hours < 12){
         timeOfDay = "morning"
+        styles.color = "#04756F"
     }else if(hours >=12 && hours <=17){
         timeOfDay = "afternoon"
+        styles.color = "#D90000"
     }else{
         timeOfDay = "night"
+        styles.color = "#8914A3"
     }
     return (
         <main>
               <p>This is where most of my content will go...</p>
             <h1>Hello {`${firstName} ${lastName}`}</h1>
             <h1>It is currently about {date.getHours() % 12} o'clock!</h1>
-    <h1>Good {timeOfDay} !</h1>
+            <h1 style={styles}>Good {timeOfDay}!</h1>
           </main>
     )
 }
