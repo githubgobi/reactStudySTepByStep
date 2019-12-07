ac819d1de97f724886fa43357fba5adff30e7fee
gobi <gobi@hartwintech.com>
2019-12-07 15:42:58 +0530

    lesson -8 todo phase 3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 // import ReactDom from 'react-dom';
 
-import Joke from './components/Joke';
-import jokeData from './data/jokeData';
+// import Joke from './components/Joke';
+// import jokeData from './data/jokeData';
 
 
-import Product from './components/Product';
-import productData from './data/productData';
+// import Product from './components/Product';
+// import productData from './data/productData';
+
+
+import Todo from './components/ToDo';
+import todoData from './data/todoData';
+
+import './App.css';
 
 function App(){
-  const jokeComponents = jokeData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} /> )
-  const productComponents = productData.map(product => <Product key={product.id} product={product} /> )
+  // const jokeComponents = jokeData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} /> )
+  // const productComponents = productData.map(product => <Product key={product.id} product={product} /> )
+  const todoItems = todoData.map(item => <Todo key={item.id} item={item}/>)
   return (
-    <div>
-           {jokeComponents}   
-           {productComponents}
+    <div className="todo-list">
+           {/* {jokeComponents}   
+           {productComponents} */}
+           {todoItems}
     </div>
   )
 }
